e93147c85d1ccac1b85cc5b7e3adb197d44b6e56
takashi-sasaki3 <t.sasaki.fujiyacamera@gmail.com>
2018-11-07 13:00:11 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/takashi/sasaki/javalog/Animal.java
@@ -0,0 +1,8 @@
+package jp.techacademy.takashi.sasaki.javalog;
+
+public abstract class Animal {
+    protected String name;
+    protected int age;
+
+    abstract public void say();
+}
---
+++ app/src/main/java/jp/techacademy/takashi/sasaki/javalog/Thinkable.java
@@ -0,0 +1,5 @@
+package jp.techacademy.takashi.sasaki.javalog;
+
+public interface Thinkable {
+    public void think();
+}
